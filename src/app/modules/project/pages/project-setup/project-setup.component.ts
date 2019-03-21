01f87714ecb2fbae315d90c9fe1a8509ea74da13
Neeraj Negi <neeraj.negi174@gmail.com>
2019-03-21 09:48:02 +0530

    Project Details component started

--- src/app/modules/project/pages/project-setup/project-setup.component.ts
+++ src/app/modules/project/pages/project-setup/project-setup.component.ts
@@ -8,6 +8,11 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class ProjectSetupComponent implements OnInit {
 
+  detailsTab: boolean = true;
+  milestonesTab: boolean = false;
+  teamsTab: boolean = false;
+  summaryTab: boolean = false;
+
   constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -16,17 +21,37 @@ export class ProjectSetupComponent implements OnInit {
 
   goToDetails(): void {
     this.router.navigate(['./details'], { relativeTo: this.route });
+    
+    this.milestonesTab = false;
+    this.teamsTab = false;
+    this.summaryTab = false;
+    this.detailsTab = true;
   }
 
   goToMilestones(): void {
     this.router.navigate(['./milestones'], { relativeTo: this.route });
+
+    this.milestonesTab = true;
+    this.teamsTab = false;
+    this.summaryTab = false;
+    this.detailsTab = false;
   }
 
   goToTeams(): void {
     this.router.navigate(['./teams'], { relativeTo: this.route });
+    
+    this.milestonesTab = false;
+    this.teamsTab = true;
+    this.summaryTab = false;
+    this.detailsTab = false;
   }
 
   goToSummary(): void {
     this.router.navigate(['./summary'], { relativeTo: this.route });
+
+    this.milestonesTab = false;
+    this.teamsTab = false;
+    this.summaryTab = true;
+    this.detailsTab = false;
   }
 }
